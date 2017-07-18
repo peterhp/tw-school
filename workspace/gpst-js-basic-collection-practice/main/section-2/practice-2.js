5366b8f8d993acae463ced060a6bc1f31695c7c7
Sora <sora_lsh@163.com>
2017-07-18 15:40:03 +0800

    Complete code for section-2 practice-2

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
@@ -1,11 +1,26 @@
 'use strict';
 
+const map2Array = require('../../main/section-2/practice-1').map2Array;
+
 function getElemCount(str) {
     return (1 == str.length ? 1 : parseInt(str.substr(2), 10));
 }
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    var map = new Object();
+
+    for (var i = 0; i < collection.length; ++i) {
+        var c = collection[i].charAt(0);
+        var count = getElemCount(collection[i]);
+
+        if (typeof map[c] == "undefined") {
+            map[c] = count;
+        } else {
+            map[c] += count;
+        }
+    }
+
+  return map2Array(map);
 }
 
 module.exports = {

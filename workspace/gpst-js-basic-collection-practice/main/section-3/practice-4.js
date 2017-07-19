6d4db4996a8dada59c32e15bd39e22fba083d124
Sora <sora_lsh@163.com>
2017-07-19 10:12:06 +0800

    Complete code for section-3 practice-4

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-4.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-4.js
@@ -1,5 +1,58 @@
 'use strict';
 
+function genObject(str) {
+  var count = 1;
+
+  if (str.length > 1) {
+    count = parseInt(str.substr(2), 10);
+  }
+
+  return {key: str[0], count: count};
+}
+
+function find(elemArr, key) {
+  var obj = null;
+  for (var i = 0; i < elemArr.length; ++i) {
+    var elem = elemArr[i];
+    if (elem.key === key) {
+      obj = elem;
+    }
+  }
+  return obj;
+}
+
+function each3Sub1(num) {
+    return (num - Math.floor(num / 3));
+}
+
+function countElem(collection) {
+  var elemArr = [];
+
+  for (var i = 0; i < collection.length; ++i) {
+    var elem = genObject(collection[i]);
+
+    var obj = find(elemArr, elem.key);
+    if (!obj) {
+      elemArr.push(elem);
+    } else {
+      obj.count += elem.count;
+    }
+  }
+
+  return elemArr;
+}
+
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var elemArr = countElem(collectionA);
+
+  for (var i = 0; i < elemArr.length; ++i) {
+    var elem = elemArr[i];
+    for (var j = 0; j < objectB.value.length; ++j) {
+      if (elem.key === objectB.value[j]) {
+        elem.count = each3Sub1(elem.count);
+      }
+    }
+  }
+
+  return elemArr;
 }

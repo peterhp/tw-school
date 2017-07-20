aefddce19ffbb484d8dd5b616cebf7c78d060d32
Sora <sora_lsh@163.com>
2017-07-20 22:26:48 +0800

    Print receipt for cart, [expected: 15min, actual: 19min]

--- workspace/pos/v2/main/main.js
+++ workspace/pos/v2/main/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
+
+function printReceipt(tags) {
+    const cart = parseCartFromTags(tags);
+    const receipt = genReceipt(cart);
+    console.log(receipt.toString());
+}
--- workspace/pos/v2/main/model/receipt.js
+++ workspace/pos/v2/main/model/receipt.js
@@ -61,6 +61,42 @@ class Receipt {
         this.date = new Date();
     }
 
+    toString() {
+        let info = `***<没钱赚商店>收据***\n`;
+
+        info += this._getDateString(this.date) + `\n`;
+        info += `----------------------\n`;
+
+        for (let itemReceipt of this.items) {
+            info += this._getItemString(itemReceipt) + `\n`;
+        }
+        info += `----------------------\n`;
+
+        info += `总计：${this.totalCost.toFixed(2)}(元)\n`;
+        info += `节省：${this.totalSaving.toFixed(2)}(元)\n`;
+
+        info += `**********************`;
+
+        return info;
+    }
+
+    _getDateString(date) {
+        const dateDigitToString = num => (num < 10 ? `0${num}` : num);
+        const year = dateDigitToString(date.getFullYear()),
+            month = dateDigitToString(date.getMonth() + 1),
+            day = dateDigitToString(date.getDate()),
+            hour = dateDigitToString(date.getHours()),
+            minute = dateDigitToString(date.getMinutes()),
+            second = dateDigitToString(date.getSeconds()),
+            formattedDateString = `${year}年${month}月${day}日 ${hour}:${minute}:${second}`;
+        return `打印时间：${formattedDateString}`;
+    }
+
+    _getItemString(itemReceipt) {
+        const commodity = itemReceipt.item.commodity;
+        return `名称：${commodity.name}，数量：${itemReceipt.item.quantity}${commodity.unit}，` +
+            `单价：${commodity.price.toFixed(2)}(元)，小计：${itemReceipt.cost.toFixed(2)}(元)`;
+    }
 }
 
 function genReceipt(cart) {

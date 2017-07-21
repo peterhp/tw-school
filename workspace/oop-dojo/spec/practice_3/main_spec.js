ea86d53a3f0cb4fd597deaafb26575183ce0b824
Sora <sora_lsh@163.com>
2017-07-21 16:15:19 +0800

    practice_3 pass, [expected: 10min, actual: 12min]

--- workspace/oop-dojo/spec/practice_3/main_spec.js
+++ workspace/oop-dojo/spec/practice_3/main_spec.js
@@ -1,7 +1,15 @@
 "use strict";
 
+import {Person, Student} from "../../main/practice_3"
+
 describe("OO入门", function(){
     it("覆盖父类的方法-1", function(){
-    
+        const tom = new Person("Tom", 21);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old.");
+
+        const stu = new Student("Tom", 21, 2);
+        expect(stu.introduce()).toEqual(
+            "I am a Student. I am at Class 2.");
     });
 });

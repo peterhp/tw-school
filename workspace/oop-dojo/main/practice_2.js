9a42850f2ec3d24b3da8a627239d4750637598b7
Sora <sora_lsh@163.com>
2017-07-21 15:53:10 +0800

    practice_2 pass, [expected: 10min, actual: 3min]

---
+++ workspace/oop-dojo/main/practice_2.js
@@ -0,0 +1,11 @@
+
+const Person = function (name, age) {
+    this.name = name;
+    this.age = age;
+};
+
+Person.prototype.introduce = function () {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+}
+
+module.exports = Person;
--- workspace/oop-dojo/spec/practice_2/main_spec.js
+++ workspace/oop-dojo/spec/practice_2/main_spec.js
@@ -1,7 +1,11 @@
 "use strict";
 
+import Person from "../../main/practice_2"
+
 describe("OO入门", function(){
     it("写一个类-2", function(){
-
+        const tom = new Person("Tom", 21);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old.");
     });
 });

cab288fef39fc74842c21c7f26d3286d8d040df6
Sora <sora_lsh@163.com>
2017-07-21 16:48:06 +0800

    practice_5 pass, [expected: 10min, actual: 12min]

--- workspace/oop-dojo/spec/practice_5/main_spec.js
+++ workspace/oop-dojo/spec/practice_5/main_spec.js
@@ -1,7 +1,21 @@
 "use strict";
 
+import {Person, Student, Worker} from "../../main/practice_5"
+
 describe("OO入门", function(){
     it("覆盖同时复用父类的方法", function(){
+        const per = new Person("Tom", 21);
+        expect(per.introduce()).toEqual(
+            "My name is Tom. I am 21 years old.");
 
+        const stu = new Student("Tom", 21, 2);
+        expect(stu.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am at Class 2.");
+
+        const worker = new Worker("Tom", 21);
+        expect(worker.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Worker. I have a job.");
     });
 });

dd90e7ae22bf0f4085532b27a46a07daec66b170
Sora <sora_lsh@163.com>
2017-07-22 09:51:43 +0800

    practice_8 test_case_2 pass

--- workspace/oop-dojo/main/practice_8.js
+++ workspace/oop-dojo/main/practice_8.js
@@ -10,6 +10,30 @@ class Person {
     }
 }
 
+class Class {
+    constructor(classNum) {
+        this._number = classNum;
+    }
+
+    get number() {
+        return this._number;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, _class) {
+        super(name, age);
+        this.class = _class;
+    }
+
+    introduce() {
+        return `${super.introduce()} ` +
+            `I am a Student. I am at Class ${this.class.number}.`;
+    }
+}
+
 module.exports = {
-    Person
+    Person,
+    Class,
+    Student
 };
--- workspace/oop-dojo/spec/practice_8/main_spec.js
+++ workspace/oop-dojo/spec/practice_8/main_spec.js
@@ -1,15 +1,22 @@
 "use strict";
 
-import {Person} from "../../main/practice_8";
+import {Person, Class, Student} from "../../main/practice_8";
 
 describe("OO入门", function(){
     it("对象之间的交互-2", function(){
 
     });
 
-    it("Case 1: test person class.", function () {
+    it("Case 1: test Person class.", function () {
        const tom = new Person("Tom", 21);
        expect(tom.introduce()).toEqual(
            "My name is Tom. I am 21 years old.");
+    });
+
+    it ("Case 2: test Student class.", function () {
+       const tom = new Student("Tom", 21, new Class(2));
+       expect(tom.introduce()).toEqual(
+           "My name is Tom. I am 21 years old. " +
+           "I am a Student. I am at Class 2.");
     });
 });

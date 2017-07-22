55d2dd92eacf20639ecae9377cc431d05491fa63
Sora <sora_lsh@163.com>
2017-07-22 09:39:42 +0800

    practice_7 test_case_1 pass

--- workspace/oop-dojo/spec/practice_8/main_spec.js
+++ workspace/oop-dojo/spec/practice_8/main_spec.js
@@ -1,7 +1,15 @@
 "use strict";
 
+import {Person} from "../../main/practice_8";
+
 describe("OO入门", function(){
     it("对象之间的交互-2", function(){
 
+    });
+
+    it("Case 1: test person class.", function () {
+       const tom = new Person("Tom", 21);
+       expect(tom.introduce()).toEqual(
+           "My name is Tom. I am 21 years old.");
     });
 });

0ca980090fa7f3d6449f57451379fbb1b8d51cb3
Sora <sora_lsh@163.com>
2017-07-22 18:53:20 +0800

    practice_9 test_case_2 pass

--- workspace/oop-dojo/main/practice_9.js
+++ workspace/oop-dojo/main/practice_9.js
@@ -15,6 +15,44 @@ class Person {
     }
 }
 
+class Class {
+    constructor(_number) {
+        this._number = _number;
+    }
+
+    get number() {
+        return this._number;
+    }
+
+    assignLeader(stu) {
+        this._leader = stu;
+    }
+
+    isLeader(stu) {
+        return ((typeof this._leader != "undefined") &&
+            (stu.isSameTo(this._leader)));
+    }
+}
+
+class Student extends Person {
+    constructor(_id, _name, _age, _class) {
+        super(_id, _name, _age);
+        this._class = _class;
+    }
+
+    introduce() {
+        let self = `${super.introduce()} I am a Student. `;
+        if (this._class.isLeader(this)) {
+            self += `I am Leader of Class ${this._class.number}.`;
+        } else {
+            self += `I am at Class ${this._class.number}.`;
+        }
+        return self;
+    }
+}
+
 module.exports = {
-    Person
+    Person,
+    Class,
+    Student
 };
--- workspace/oop-dojo/spec/practice_9/main_spec.js
+++ workspace/oop-dojo/spec/practice_9/main_spec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {Person} from "../../main/practice_9";
+import {Person, Class, Student} from "../../main/practice_9";
 
 describe("OO入门", function(){
     it("对象之间的交互-3", function(){
@@ -14,5 +14,23 @@ describe("OO入门", function(){
 
         // Assume that Tom changes his name to Tony at his 23 years old.
         expect(tom.isSameTo(new Person("001", "Tony", 23))).toEqual(true);
+    });
+
+    it("Case 2: test Student.", () => {
+        const class2 = new Class(2);
+        const tom = new Student("001", "Tom", 21, class2);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am at Class 2.");
+
+        class2.assignLeader(new Student("002", "Tony", 22, class2));
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am at Class 2.");
+
+        class2.assignLeader(tom);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am Leader of Class 2.");
     });
 });

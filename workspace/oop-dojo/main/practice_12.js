c972e14bf093fb0b7a70ba5fdaa34d7724c09dfe
Sora <sora_lsh@163.com>
2017-07-23 19:39:54 +0800

    practice_12 test_case_2 pass

--- workspace/oop-dojo/main/practice_12.js
+++ workspace/oop-dojo/main/practice_12.js
@@ -15,6 +15,69 @@ class Person {
     }
 }
 
+class Class {
+    constructor(_number) {
+        this._number = _number;
+        this._leader = null;
+        this._students = [];
+    }
+
+    get number() {
+        return this._number;
+    }
+
+    isIn(stu) {
+        for (let com of this._students) {
+            if (com.isSameTo(stu)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    appendMember(stu) {
+        if (!this.isIn(stu)) {
+            this._students.push(stu);
+            stu.class = this;
+        }
+    }
+
+    assignLeader(stu) {
+        if (this.isIn(stu)) {
+            this._leader = stu;
+        } else {
+            console.log("It is not one of us.");
+        }
+    }
+
+    isLeader(stu) {
+        return (this._leader && this._leader.isSameTo(stu));
+    }
+}
+
+class Student extends Person {
+    constructor(_id, _name, _age, _class) {
+        super(_id, _name, _age);
+        _class.appendMember(this);
+    }
+
+    set class(_class) {
+        this._class = _class;
+    }
+
+    introduce() {
+        let self = `${super.introduce()} I am a Student. `;
+        if (this._class.isLeader(this)) {
+            self += `I am Leader of Class ${this._class.number}.`;
+        } else {
+            self += `I am at Class ${this._class.number}.`;
+        }
+        return self;
+    }
+}
+
 module.exports = {
-    Person
+    Person,
+    Class,
+    Student
 };
--- workspace/oop-dojo/spec/practice_12/main_spec.js
+++ workspace/oop-dojo/spec/practice_12/main_spec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {Person} from "../../main/practice_12";
+import {Person, Class, Student} from "../../main/practice_12";
 
 describe("OO入门", function(){
     it("对象之间的交互-6", function(){
@@ -14,5 +14,29 @@ describe("OO入门", function(){
 
         expect(tom.isSameTo(new Person("001", "Tom", 22))).toEqual(true);
         expect(tom.isSameTo(new Person("002", "Tom", 20))).toEqual(false);
+    });
+
+    it("Case 2: Student class.", () => {
+        const class1 = new Class(1);
+        const class2 = new Class(2);
+
+        const tom = new Student("001", "Tom", 21, class2);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am at Class 2.");
+
+        class2.assignLeader(tom);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am Leader of Class 2.");
+
+        spyOn(console, "log");
+        const jim = new Student("002", "Jim", 22, class1);
+        class2.assignLeader(jim);
+        expect(console.log).toHaveBeenCalledWith(
+            "It is not one of us.");
+        expect(jim.introduce()).toEqual(
+            "My name is Jim. I am 22 years old. " +
+            "I am a Student. I am at Class 1.");
     });
 });

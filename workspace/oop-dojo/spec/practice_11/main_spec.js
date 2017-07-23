d15d5ef9fbf7182b4678a4b37429692ce5d38793
Sora <sora_lsh@163.com>
2017-07-23 17:05:28 +0800

    practice_11 test_case_2 pass

--- workspace/oop-dojo/spec/practice_11/main_spec.js
+++ workspace/oop-dojo/spec/practice_11/main_spec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {Person} from "../../main/practice_11";
+import {Person, Class, Student} from "../../main/practice_11";
 
 describe("OO入门", function(){
     it("对象之间的交互-5", function(){
@@ -16,4 +16,25 @@ describe("OO入门", function(){
         expect(tom.isSameTo(new Person("002", "Jim", 20))).toEqual(false);
     });
 
+    it("Case 2: Student class.", () => {
+        const class2 = new Class(2);
+        const tom = new Student("001", "Tom", 21, class2);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am at Class 2.");
+
+        class2.assignLeader(tom);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old. " +
+            "I am a Student. I am Leader of Class 2.");
+
+        spyOn(console, "log");
+        const jim = new Student("002", "Jim", 21, new Class(1));
+        class2.assignLeader(jim);
+        expect(console.log).toHaveBeenCalledWith(
+            "It is not one of us.");
+        expect(jim.introduce()).toEqual(
+            "My name is Jim. I am 21 years old. " +
+            "I am a Student. I am at Class 1.");
+    });
 });

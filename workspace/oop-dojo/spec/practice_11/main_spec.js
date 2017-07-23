00beb0de59e31dd4eeab5568567f73d082e38c23
Sora <sora_lsh@163.com>
2017-07-23 16:50:14 +0800

    practice_11 test_case_1 pass

--- workspace/oop-dojo/spec/practice_11/main_spec.js
+++ workspace/oop-dojo/spec/practice_11/main_spec.js
@@ -1,7 +1,19 @@
 "use strict";
 
+import {Person} from "../../main/practice_11";
+
 describe("OO入门", function(){
     it("对象之间的交互-5", function(){
 
     });
+
+    it("Case 1: Person class.", () => {
+        const tom = new Person("001", "Tom", 21);
+        expect(tom.introduce()).toEqual(
+            "My name is Tom. I am 21 years old.");
+
+        expect(tom.isSameTo(new Person("001", "Tom", 22))).toEqual(true);
+        expect(tom.isSameTo(new Person("002", "Jim", 20))).toEqual(false);
+    });
+
 });

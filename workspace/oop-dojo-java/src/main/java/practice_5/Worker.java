11b0737e1d492f9be27392515ec30e22184a4add
Sora <sora_lsh@163.com>
2017-07-24 16:35:34 +0800

    practice_5 final_test pass, [expected: 15min, actual: 13min]

---
+++ workspace/oop-dojo-java/src/main/java/practice_5/Worker.java
@@ -0,0 +1,16 @@
+package practice_5;
+
+/**
+ * Created by Shli on 24/07/2017.
+ */
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Worker. I have a job.",
+                this.basicIntroduce());
+    }
+}

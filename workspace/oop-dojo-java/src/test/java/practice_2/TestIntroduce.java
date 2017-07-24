7f4b06fdb76f7b6be0073d4215b04146f65725b8
Sora <sora_lsh@163.com>
2017-07-24 15:27:13 +0800

    practice_2 test_1 pass

---
+++ workspace/oop-dojo-java/src/test/java/practice_2/TestIntroduce.java
@@ -0,0 +1,18 @@
+package practice_2;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Shli on 24/07/2017.
+ */
+public class TestIntroduce {
+
+    @Test
+    public void should_introduce_person() throws Exception {
+        Person tom = new Person("Tom", 21);
+        assertThat(tom.introduce(), is("My name is Tom. I am 21 years old."));
+    }
+}

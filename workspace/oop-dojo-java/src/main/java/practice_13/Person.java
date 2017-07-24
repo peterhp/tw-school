d217bb08ef3b38166fc7783a1340074c1f496b05
Sora <sora_lsh@163.com>
2017-07-24 22:43:13 +0800

    practice_13 test_1 pass

---
+++ workspace/oop-dojo-java/src/main/java/practice_13/Person.java
@@ -0,0 +1,21 @@
+package practice_13;
+
+/**
+ * Created by Shli on 24/07/2017.
+ */
+public class Person {
+    private final String id;
+    private final String name;
+    private final int age;
+
+    public Person(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.",
+                this.name, this.age);
+    }
+}
---
+++ workspace/oop-dojo-java/src/test/java/practice_13/TestIntroduce.java
@@ -0,0 +1,17 @@
+package practice_13;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Shli on 24/07/2017.
+ */
+public class TestIntroduce {
+    @Test
+    public void should_introduce_person() throws Exception {
+        Person tom = new Person("001", "Tom", 21);
+        assertThat(tom.introduce(), is("My name is Tom. I am 21 years old."));
+    }
+}

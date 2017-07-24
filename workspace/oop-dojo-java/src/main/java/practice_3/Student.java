fa36daae96f9fdabcd9a70f390332ee99cb31117
Sora <sora_lsh@163.com>
2017-07-24 16:02:23 +0800

    practice_3 final_test pass, [expected: 15min, actual: 21min]

---
+++ workspace/oop-dojo-java/src/main/java/practice_3/Student.java
@@ -0,0 +1,19 @@
+package practice_3;
+
+/**
+ * Created by Shli on 24/07/2017.
+ */
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Student. I am at Class %d.",
+                super.introduce(), this.klass);
+    }
+}

ae30a326e6d5f3ab097c5d691c1193a2891bd847
Sora <sora_lsh@163.com>
2017-07-24 16:16:14 +0800

    practice_4 test_2 pass

---
+++ workspace/oop-dojo-java/src/main/java/practice_4/Student.java
@@ -0,0 +1,19 @@
+package practice_4;
+
+/**
+ * Created by Shli on 24/07/2017.
+ */
+public class Student extends Person {
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Student. I am at Class %d.",
+                super.introduce(), this.klass);
+    }
+}

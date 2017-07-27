1aaf23dd9978132e4f0deb8a296b32edc43b80ad
Sora <sora_lsh@163.com>
2017-07-27 15:30:42 +0800

    Clean the project

--- workspace/student-score-ms/src/main/java/exception/InvalidStudentFormatException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-/**
- * Created by Shli on 26/07/2017.
- */
-public class InvalidStudentFormatException extends Exception {
-}
--- workspace/student-score-ms/src/main/java/exception/StudentNotExistInClassException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-/**
- * Created by Shli on 27/07/2017.
- */
-public class StudentNotExistInClassException extends Exception {
-}

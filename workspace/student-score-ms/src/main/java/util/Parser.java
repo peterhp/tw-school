920a1435d1c09c21725cfbe24042481b8bd79346
Sora <sora_lsh@163.com>
2017-07-30 14:05:54 +0800

    [lsh] parse student from its info string, [expected: 20min, actual: 17min]

---
+++ workspace/student-score-ms/src/main/java/util/Parser.java
@@ -0,0 +1,24 @@
+package util;
+
+import data.Student;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Shli on 30/07/2017.
+ */
+public class Parser {
+    private static final String REG_STUDENT_INFORMATION =
+            "([^，]+)，(\\w+)，数学：(\\d+)，语文：(\\d+)，英语：(\\d+)，编程：(\\d+)";
+
+    public static Student parseStudent(String stuInfo) {
+        Pattern pattern = Pattern.compile(REG_STUDENT_INFORMATION);
+        Matcher matcher = pattern.matcher(stuInfo);
+
+        matcher.matches();
+        return new Student(matcher.group(2), matcher.group(1),
+                Integer.valueOf(matcher.group(3)), Integer.valueOf(matcher.group(4)),
+                Integer.valueOf(matcher.group(5)), Integer.valueOf(matcher.group(6)));
+    }
+}
---
+++ workspace/student-score-ms/src/test/java/ParserTest.java
@@ -0,0 +1,28 @@
+import data.Student;
+import org.junit.Test;
+import util.Parser;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Shli on 30/07/2017.
+ */
+public class ParserTest {
+    @Test
+    public void should_parse_student_from_txt_containing_student_info() throws Exception {
+        // given
+        String stuInfo = "张三，S003，数学：75，语文：95，英语：80，编程：80";
+
+        // when
+        Student stu = Parser.parseStudent(stuInfo);
+
+        // then
+        assertThat(stu.getId(), is("S003"));
+        assertThat(stu.getName(), is("张三"));
+        assertThat(stu.getMathScore(), is(75));
+        assertThat(stu.getChineseScore(), is(95));
+        assertThat(stu.getEnglishScore(), is(80));
+        assertThat(stu.getProgramScore(), is(80));
+    }
+}

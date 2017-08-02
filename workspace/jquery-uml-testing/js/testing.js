8ee9db0241115a44445b763057415016fd1dce72
Sora <sora_lsh@163.com>
2017-08-02 17:27:26 +0800

    [lsh] get answers for test section 2, [expected: 10min, actual: 13min]

--- workspace/jquery-uml-testing/js/testing.js
+++ workspace/jquery-uml-testing/js/testing.js
@@ -4,9 +4,11 @@
 
 $(document).ready(function () {
     const student = getStudentInfo();
-    student.answer.push(getAnswerForTestItem1());
 
-    console.log(student);
+    student.answer.t1 = getAnswerForTestItem1();
+    student.answer.t2 = getAnswerForTestItem2();
+
+    console.log(student.answer.t2);
 });
 
 const getStudentInfo = function () {
@@ -14,14 +16,20 @@ const getStudentInfo = function () {
         id: $("#stu-id").val(),
         name: $("#stu-name").val(),
         klass: $("#stu-class").val(),
-        answer: []
+        answer: {}
     };
 };
 
 const getAnswerForTestItem1 = function () {
-    const answers = [];
-    for (let elem of $(".t1")) {
-        answers.push(elem.value);
-    }
-    return answers;
+    return {
+        q1: $(".t1-1").val(),
+        q2: $(".t1-2").map(function () {return $(this).val();})
+    };
+};
+
+const getAnswerForTestItem2 = function () {
+    return {
+        q1: $("input[name='t2.1']:checked").val(),
+        q2: $("input[name='t2.2']:checked").val()
+    };
 };

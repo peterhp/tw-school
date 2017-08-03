10b7251368826a0d6761cb2245f0ae91d3b51b2e
Sora <sora_lsh@163.com>
2017-08-04 01:32:22 +0800

    [lsh] display report based on student ids, [expected: 20min, actual: 23min]

--- workspace/web-student-management/js/main-layout.js
+++ workspace/web-student-management/js/main-layout.js
@@ -14,10 +14,37 @@ const retrieveLayoutElements = function () {
 
 const KEY_CURRENT_SID = "cur-sid";
 const KEY_STUDENT_LIST = "stu-list";
+const KEY_REPORT = "stu-report";
 
 const initStorage = function () {
    initDataInStorage(KEY_CURRENT_SID, "100");
    initDataInStorage(KEY_STUDENT_LIST, "[]");
+   initDataInStorage(KEY_REPORT, JSON.stringify({
+       items: [
+           {
+               sid: "S003",
+               name: "San",
+               math: 75,
+               chinese: 95,
+               english: 80,
+               program: 80,
+               total: 330,
+               average: 82.5
+           },
+           {
+               sid: "S004",
+               name: "Si",
+               math: 85,
+               chinese: 80,
+               english: 70,
+               program: 90,
+               total: 325,
+               average: 81.25
+           }
+       ],
+       average: 327.5,
+       median: 327.5
+   }));
 };
 
 const initDataInStorage = function (key, value) {
--- workspace/web-student-management/js/report.js
+++ workspace/web-student-management/js/report.js
@@ -2,9 +2,52 @@
  * Created by Shli on 04/08/2017.
  */
 
+const TABLE_HEADER = "<tr>" +
+    "<th>sid</th><th>name</th>" +
+    "<th>math</th><th>chinese</th>" +
+    "<th>english</th><th>program</th>" +
+    "<th>total</th><th>average</th>" +
+    "</tr>";
+
 $(document).ready(function () {
+    fillReportData(null);
+
     $("form").submit(function () {
         event.preventDefault();
 
+        const input = $("input[name='sid-list']").val();
+        if (input.length > 0) {
+            fillReportData(getReportForStudents(input));
+        }
     });
 });
+
+const fillReportData = function (report) {
+    let tblView = TABLE_HEADER;
+    let average = 0;
+    let median = 0;
+
+    if (report != null) {
+        tblView += report.items.map(item => generateStudentScoreView(item)).join("");
+        average = report.average;
+        median = report.median;
+    }
+
+    $("table").html(tblView);
+    $("#average-total").text(average.toString());
+    $("#median-total").text(median.toString());
+};
+
+const getReportForStudents = function (sids) {
+    return (sids === "S003,S004" ? JSON.parse(
+            getDataInStorage(KEY_REPORT)) : null);
+};
+
+const generateStudentScoreView = function (item) {
+    return `<tr>` +
+        `<td>${item.sid}</td><td>${item.name}</td>` +
+        `<td>${item.math}</td><td>${item.chinese}</td>` +
+        `<td>${item.english}</td><td>${item.program}</td>` +
+        `<td>${item.total}</td><td>${item.average}</td>` +
+        `</tr>`;
+};

8b9c89581b93ae75d3967b1b1c1cb7f595a1408a
Sora <sora_lsh@163.com>
2017-08-03 22:10:42 +0800

    [lsh] submit student info and save it in local storage, [expected: 25min, actual: 34min]

--- workspace/web-student-management/js/add-student.js
+++ workspace/web-student-management/js/add-student.js
@@ -16,10 +16,40 @@ $(document).ready(function () {
            class: "Please input your class."
        },
 
-       errorLabelContainer: $("#msg"),
+       errorLabelContainer: $("#err"),
 
        submitHandler: function (form) {
-           form.submit();
+           const student = getStudentInformation();
+           saveStudentInLocalStorage(student);
+
+           $("#msg").text(`Succeed to append student ${student.name}.`);
+
+           //form.submit();
+           form.reset();
        }
    });
 });
+
+const saveStudentInLocalStorage = function (newStudent) {
+    const students = JSON.parse(getDataInStorage(KEY_STUDENT_LIST));
+
+    students.push(newStudent);
+    saveDataInStorage(KEY_STUDENT_LIST, JSON.stringify(students));
+};
+
+const getStudentInformation = function () {
+    const inputs = $("form").serializeArray();
+
+    const student = {sid: generateSidForNewStudent()};
+    for (let kv of inputs) {
+        student[kv.name] = kv.value;
+    }
+
+    return student;
+};
+
+const generateSidForNewStudent = function () {
+    const sid = parseInt(getDataInStorage(KEY_CURRENT_SID)) + 1;
+    saveDataInStorage(KEY_CURRENT_SID, sid.toString());
+    return `S${sid}`;
+};
--- workspace/web-student-management/js/main-layout.js
+++ workspace/web-student-management/js/main-layout.js
@@ -3,6 +3,37 @@
  */
 
 $(function () {
-   $("header").load("header.html");
-   $("footer").load("footer.html");
+   retrieveLayoutElements();
+   initStorage();
 });
+
+const retrieveLayoutElements = function () {
+    $("header").load("header.html");
+    $("footer").load("footer.html");
+};
+
+const KEY_CURRENT_SID = "cur-sid";
+const KEY_STUDENT_LIST = "stu-list";
+
+const initStorage = function () {
+   initDataInStorage(KEY_CURRENT_SID, "100");
+   initDataInStorage(KEY_STUDENT_LIST, "[]");
+};
+
+const initDataInStorage = function (key, value) {
+    if (!isExistInStorage(key)) {
+       saveDataInStorage(key, value);
+    }
+};
+
+const isExistInStorage = function (key) {
+    return (localStorage.getItem(key) != null);
+};
+
+const saveDataInStorage = function (key, value) {
+    localStorage.setItem(key, value);
+};
+
+const getDataInStorage = function (key) {
+    return localStorage.getItem(key);
+};

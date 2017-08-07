8f3ca588331e3d04e41bb54ba2237e049b13fd0d
Sora <sora_lsh@163.com>
2017-08-07 14:15:14 +0800

    Introduce spring boot framework for student score practice

---
+++ workspace/student-score-ms/src/main/java/WebApp.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Shli on 07/08/2017.
+ */
+@SpringBootApplication
+public class WebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+}

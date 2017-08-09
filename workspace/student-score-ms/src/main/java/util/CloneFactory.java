f923bb5ae237427aa82c7ec07c8c698de57cc4c0
Sora <sora_lsh@163.com>
2017-08-09 09:36:55 +0800

    [lsh] create deep clone for student list, [expected: 15min, actual: 13min]

--- workspace/student-score-ms/src/main/java/util/CloneFactory.java
+++ workspace/student-score-ms/src/main/java/util/CloneFactory.java
@@ -2,6 +2,9 @@ package util;
 
 import data.Student;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by Shli on 09/08/2017.
  */
@@ -9,5 +12,11 @@ public class CloneFactory {
 
     public static Student cloneStudent(Student student) {
         return student.clone();
+    }
+
+    public static List<Student> cloneStudentList(List<Student> studentList) {
+        return studentList.stream()
+                .map(student -> student.clone())
+                .collect(Collectors.toList());
     }
 }
--- workspace/student-score-ms/src/test/java/CloneTest.java
+++ workspace/student-score-ms/src/test/java/CloneTest.java
@@ -2,6 +2,9 @@ import data.Student;
 import org.junit.Test;
 import util.CloneFactory;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -16,11 +19,31 @@ public class CloneTest {
 
         // when
         Student copied = CloneFactory.cloneStudent(student);
+        copied.setName("er");
 
         // then
         assertThat(copied == student, is(false));
         assertThat(copied.getSid(), is(student.getSid()));
-        assertThat(copied.getName(), is(student.getName()));
         assertThat(copied.getProgramScore(), is(student.getProgramScore()));
+
+        assertThat(student.getName(), is("yi"));
+        assertThat(copied.getName(), is("er"));
+    }
+
+    @Test
+    public void should_clone_list_and_students_within_it() throws Exception {
+        // given
+        List<Student> list = Arrays.asList(
+                new Student("S001", "yi", 80, 80, 80, 80));
+
+        // when
+        List<Student> copied = CloneFactory.cloneStudentList(list);
+
+        // then
+        assertThat(copied == list, is(false));
+        assertThat(copied.size(), is(list.size()));
+
+        assertThat(copied.get(0) == list.get(0), is(false));
+        assertThat(copied.get(0).getSid(), is(list.get(0).getSid()));
     }
 }
